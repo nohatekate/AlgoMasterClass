8ae81f2bc7ec33283036e0e490cb25b5193df569
nohatekate <kateiscoding@gmail.com>
2023-06-15 13:55:45 -0400

    logAtLast5 algo practice

--- algo_practice.js
+++ algo_practice.js
@@ -14,8 +14,16 @@
 // // 6 * (6+1)/2
 // console.log(addUpTo(6))
 
+
+function logAtLeast5(n){
+    for (var i = 1; i<= Math.max(5, n); i++){
+        console.log(i)
+    }
+}
+logAtLeast5(100)
+
 // checking time
-let t1 = performance.now();
-addUpTo(1000000000);
-let t2 = performance.now();
-console.log(`Time Elapsed: ${(t2 - t1) / 1000 } seconds.`)
+// let t1 = performance.now();
+// addUpTo(1000000000);
+// let t2 = performance.now();
+// console.log(`Time Elapsed: ${(t2 - t1) / 1000 } seconds.`)
